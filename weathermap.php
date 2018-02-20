3e2ba7848ac16a706a080a8782b9adb1f37c4377
Jessica Smith <jess@mintopia.net>
2018-02-20 22:55:27 +0000

    Initial attempt at weathermap file

---
+++ weathermap.php
@@ -0,0 +1,38 @@
+<?php
+  set_time_limit(0);
+  
+  function processWeathermaps()
+  {
+    $time = date('[H:m:s]');
+    echo "{$time} Processing weathermaps\r\n";
+    $configs = glob('/configs/*.conf');
+    foreach ($configs as $config) {
+      processWeathermap(str_replace('.conf', '', $config));
+    }
+  }
+  
+  function processWeathermap($file)
+  {
+    $time = date('[H:m:s]');
+    echo "{$time} Processing {$file}\r\n;
+    $path = "/output/{$file}";
+    $archiveFileName = date('YmdHis-{$file}.png');
+    
+    // Execute weathermap
+    exec("/usr/bin/php opt/network-weathermap/weathermap --config /configs/{$file}.conf --output {$path}/weathermap.png --htmloutput {$path}/index.html");
+    
+    // Copy weathermap to archive
+    copy($path . '/weathermap.png', $path . '/archive/' . $archiveFilename);
+  }
+
+  while (true) {
+    $start = time();
+    processWeathermaps();
+    $end = time();
+    $toSleep = 60 - ($end - $start);
+    if ($toSleep < 0) {
+      $toSleep = 0;
+    } elseif ($toSleep > 60) {
+      $toSleep = 60;
+    }
+  }
